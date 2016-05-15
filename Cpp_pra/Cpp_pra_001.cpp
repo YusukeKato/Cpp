d9325081497f9f8b6b0e945668854fa336c33bea
YusukeKato <mikan1212.lion2a4p8c3@docomo.ne.jp>
2016-05-15 15:58:41 +0900

    Cpp_pra_001.cpp
    2016.5.15
    初めてのC++
    色んな言語を体験してみる

---
+++ Cpp_pra/Cpp_pra_001.cpp
@@ -0,0 +1,24 @@
+/*
+ * Cpp_pra_001.cpp
+ * Yusuke_Kato
+ * 2016.5.15
+ */
+ 
+/*]
+ * 初めてのC++
+ */
+ 
+#include <iostream>//ヘッダファイルをインクルード
+using namespace std;
+/*
+ * "cout"はもともと、"std.cout"らしい。
+ * それを省略するために、先に"std"を付けておく。
+ */
+
+int main(void)
+{
+	cout << ' ' << 1 << '\n';//標準出力
+	cout << ' ' << 2 << '\n';//standard_output
+	
+	return 0;
+}
