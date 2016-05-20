3375d10005400dce4c5fde09b1f38c756289e4c9
YusukeKato <mikan1212.lion2a4p8c3@docomo.ne.jp>
2016-05-20 23:56:16 +0900

    Cpp_pra_004.cpp

--- Cpp_pra/Cpp_pra_004.cpp
+++ Cpp_pra/Cpp_pra_004.cpp
@@ -20,15 +20,15 @@ int func_1(int i)
 
 int main(void)
 {
-	int num;
+	int num = 0;
 	int i;
 	
-	for(i = 0;; i++){
+	for(i = 1;num != i * 100; i++){
 	
-		num = func_1(i);
+		num += func_1(i);
 	
 		cout << "----------\n";
-		cout << " " << num << "\n";
+		cout << " num << " << num << "\n";
 	}
 	
 	return 0;
